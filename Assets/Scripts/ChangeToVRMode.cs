23b24e4aba4a4b59499902c890e9b23694977459
Ben Savage <Finnegan2100@protonmail.com>
2019-11-25 17:32:13 -0800

    Added VR mode button and functionality

---
+++ Assets/Scripts/ChangeToVRMode.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ChangeToVRMode : MonoBehaviour
+{
+    private bool isInVRMode = false;
+
+    public void Start()
+    {
+        
+    }
+
+    public void ChangeToVR()
+    {
+        if(isInVRMode)
+        {
+            MixedRealityController.Instance.SetMode(MixedRealityController.Mode.VIEWER_VR);
+        } else
+        {
+            MixedRealityController.Instance.SetMode(MixedRealityController.Mode.HANDHELD_AR);
+        }
+      
+
+        isInVRMode = !isInVRMode;
+    }
+}
