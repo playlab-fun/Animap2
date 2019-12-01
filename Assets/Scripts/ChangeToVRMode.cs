5cd722b725d9cf19a211004313311d6c4e4dd268
Ben Savage <Finnegan2100@protonmail.com>
2019-12-01 13:34:32 -0800

    Increased size of animal models, turned animal velocity off when app runs

--- Assets/Scripts/ChangeToVRMode.cs
+++ Assets/Scripts/ChangeToVRMode.cs
@@ -6,7 +6,15 @@ using Vuforia;
 public class ChangeToVRMode : MonoBehaviour
 {
     private bool isInVRMode = false;
+
     public GameObject target;
+
+    public GameObject desertText;
+    public GameObject drySavannaText;
+    public GameObject grasslandText;
+    public GameObject mediteranneanText;
+    public GameObject moistSavannaText;
+    public GameObject rainforestText;
 
     public void ChangeToVR()
     {
@@ -15,9 +23,22 @@ public class ChangeToVRMode : MonoBehaviour
         if (!isInVRMode)
         {
             MixedRealityController.Instance.SetMode(MixedRealityController.Mode.HANDHELD_AR);
+            target.SetActive(true); 
         } else
         {
             MixedRealityController.Instance.SetMode(MixedRealityController.Mode.VIEWER_VR);
+            target.SetActive(false);
+            TurnOffAllText();
         }
+    }
+
+    private void TurnOffAllText()
+    {
+        desertText.SetActive(false);
+        drySavannaText.SetActive(false);
+        grasslandText.SetActive(false);
+        mediteranneanText.SetActive(false);
+        moistSavannaText.SetActive(false);
+        rainforestText.SetActive(false);
     }
 }
